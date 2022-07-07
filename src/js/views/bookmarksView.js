4ff866b783473eff3d31efe02e6b26c1902f0e29
mikeforscutt <mikeforscutt@gmail.com>
2022-07-07 09:45:43 +0100

    Stored bookmarks with localstorage

--- src/js/views/bookmarksView.js
+++ src/js/views/bookmarksView.js
@@ -6,6 +6,10 @@ class BookmarksView extends View {
   _parentElement = document.querySelector(".bookmarks__list");
   _errorMessage = "No bookmarks yet. Find a nice recipe and bookmark it ;)";
 
+  addHandlerRender(handler) {
+    window.addEventListener("load", handler);
+  }
+
   _generateMarkup() {
     console.log(this._data);
     return this._data
